28d17450f5d40b86db985cf262ea5430de3bcad2
Teju-28 <62064600+Teju-28@users.noreply.github.com>
2020-06-20 14:51:22 +0530

    Add files via upload

---
+++ 321810304018-Two strings equal or not.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1.Take two inputs from user and check whether they are equal or not
+
+# In[1]:
+
+
+str1=str(input("Enter the first string:"))
+str2=str(input("Enter the second string:"))
+if str1==str2:
+    print("Yes Both strings are equal")
+else:
+    print("No Both strings are not equal")
+
